6dd48439eb6c49d78c8baa10a4d191a766349f3d
Ivan <chakin.2001@mail.ru>
2022-06-08 11:06:04 +0300

    + register page + realWorldAPI

---
+++ src/views/Register.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class='auth-page'>
+    <div class='container page'>
+      <div class='row'>
+        <div class='col-md-6 offset-md-3 col-xs-12'>
+          <h1 class='text-xs-center'>Sign Up</h1>
+          <p class='text-xs-center'>
+            <router-link :to="{name: 'login'}">Need an account?</router-link>
+          </p>
+          VALIDATION ERRORS
+          <form @submit.prevent='onSubmit'>
+            <fieldset class='form-group'>
+              <input
+                class='form-control form-control-lg'
+                placeholder='Username'
+                type='text'
+                v-model='username'
+              >
+            </fieldset>
+
+            <fieldset class='form-group'>
+              <input
+                class='form-control form-control-lg'
+                placeholder='Email'
+                type='text'
+                v-model='email'
+              >
+            </fieldset>
+
+            <fieldset class='form-group'>
+              <input
+                class='form-control form-control-lg'
+                placeholder='Password'
+                type='text'
+                v-model='password'
+              >
+            </fieldset>
+            <button class='btn btn-lg btn-primary pull-xs-right' :disabled='isSubmitting'>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvRegister',
+  data() {
+    return {
+      email: '',
+      password: '',
+      username: ''
+    }
+  },
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting;
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit');
+      this.$store.dispatch('register', {
+        email: this.email,
+        username: this.username,
+        password: this.password
+      }).then(user => {
+        console.log('successfully register user', user)
+        this.$router.push({name: 'home'})
+      })
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
